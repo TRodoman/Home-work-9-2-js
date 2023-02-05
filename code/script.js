445e1fd6fe4d4c40e195b3e991bfe2caef031bee
Tatiana Rodoman <19fyfcnfcbz19@gmail.com>
2023-02-05 20:13:58 +0200

    Домашняя работа

--- code/script.js
+++ code/script.js
@@ -1,7 +1,53 @@
+const createETag = (tagName, className, text) => {
+  const tag = document.createElement (tagName);
+  tag.classList.add(className);
+  tag.textContent = text;
+
+  return tag;
+}
+
+function newPage () {
+
+  const div1 = createETag('div', "item", "" );
+  div2 = createETag('div', "item", "");
+  div3 = createETag('div', "item", "");
+  div4 = createETag('div', "item", "");
+
+  img1 = createETag('img', "slide1", "");
+  img2 = createETag('img', "slide2", "");
+  img3 = createETag('img', "slide3", "");
+  img4 = createETag('img', "slide4", "");
+  
+  a1 = createETag("a","prev", "<");
+  a2 = createETag("a", "next", ">");
+  
+  document.querySelector(".slider").appendChild(div1);
+  document.querySelector(".slider").appendChild(div2);
+  document.querySelector(".slider").appendChild(div3);
+  document.querySelector(".slider").appendChild(div4);
+  document.querySelector(".slider").appendChild(a1);
+  document.querySelector(".slider").appendChild(a2);
+
+  div1.appendChild(img1);
+  div2.appendChild(img2);
+  div3.appendChild(img3);
+  div4.appendChild(img4);
+
+}
+newPage ();
+
+document.querySelector(".slide1").src="https://new-science.ru/wp-content/uploads/2020/03/4848-4.jpg";
+document.querySelector(".slide2").src="https://universetoday.ru/wp-content/uploads/2018/10/Mercury.jpg";
+document.querySelector(".slide3").src="https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2018-12/20180913_zaa_p138_057.jpg";
+document.querySelector(".slide4").src="https://nnst1.gismeteo.ru/images/2020/07/shutterstock_1450308851-640x360.jpg";
+
 
 let slideIndex = 1;
 let timer = null;
 showSlides(slideIndex);
+
+document.querySelector(".prev").addEventListener('click',minusSlide);
+document.querySelector(".next").addEventListener('click',plusSlide);
 
 
 function plusSlide() {
@@ -20,7 +66,7 @@ function currentSlide(n) {
 function showSlides(n) {
 
   let slides = document.getElementsByClassName("item");
-  let dots = document.getElementsByClassName("slider-dots_item");
+ 
  
   if (n && n > slides.length) {
     slideIndex = 1;
@@ -32,9 +78,8 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].classList.remove("active");
-  }
+
+
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
@@ -42,7 +87,6 @@ function showSlides(n) {
   if (slideIndex <= 0) slideIndex = slides.length;
  
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].classList.add("active");
  
   afterTime(3000);
 }
